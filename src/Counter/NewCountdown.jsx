5e065f4f03af474b987c315754755ffc53799add
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-15 19:04:55 +0100

    Testing validation logic

--- src/Counter/NewCountdown.jsx
+++ src/Counter/NewCountdown.jsx
@@ -3,6 +3,8 @@ import classes from "./NewCountdown.module.css";
 
 const NewCountdown = (props) => {
   const [isFormValid, setIsFormValid] = useState(true);
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isTimeValid, setIsTimeValid] = useState(true);
 
   const name = useRef();
   const time = useRef();
@@ -39,20 +41,26 @@ const NewCountdown = (props) => {
           placeholder="Name"
           ref={name}
         />
+        {!isFormValid && (
+          <span className={classes.errorText}> A name is required</span>
+        )}
         <br />
         <label className={classes.label} htmlFor="minutes">
-          Time in minutes (max 60 minutes)
+          Time in minutes (max 240 minutes, or 4 hours)
         </label>
         <input
           className={isFormValid ? classes.input : classes.error}
           id="minutes"
           type="number"
           step="1"
-          max="60"
           min="1"
+          max="240"
           placeholder="1"
           ref={time}
         />
+        {!isFormValid && (
+          <span className={classes.errorText}> A time is required</span>
+        )}
         <br />
         <button className={classes["button-primary"]} type="submit">
           Set
