89a20bbbb33f095923e633b948dd44c1a2ff6524
Damiano Bacci <damiano.bacci@gmail.com>
2023-11-15 18:14:42 +0100

    Adding form validation logic

--- src/Counter/NewCountdown.jsx
+++ src/Counter/NewCountdown.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import classes from "./NewCountdown.module.css";
 
 const NewCountdown = (props) => {
+  const [isFormValid, setIsFormValid] = useState(true);
+
+  const name = useRef();
+  const time = useRef();
+
   const formSetHandler = (event) => {
     event.preventDefault();
-    let name = event.target.elements.name.value;
-    let wantedTime = +event.target.elements.minutes.value;
+    let enteredName = name.current.value;
+    let enteredTime = +time.current.value;
     props.onAddTimer({
       id: Math.random().toString(),
-      title: name,
-      timeSet: wantedTime,
+      title: enteredName,
+      timeSet: enteredTime,
     });
+    name.current.value = "";
+    time.current.value = "";
   };
 
   return (
@@ -25,19 +32,21 @@ const NewCountdown = (props) => {
           id="name"
           type="text"
           placeholder="Name"
+          ref={name}
         />
         <br />
         <label className={classes.label} htmlFor="minutes">
           Time in minutes (max 60 minutes)
         </label>
         <input
-          className={classes.input}
+          className={isFormValid ? classes.input : classes.error}
           id="minutes"
           type="number"
           step="1"
           max="60"
           min="1"
           placeholder="1"
+          ref={time}
         />
         <br />
         <button className={classes["button-primary"]} type="submit">
